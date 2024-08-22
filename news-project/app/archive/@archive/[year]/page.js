b217ddbf8368d1359ebd9084cf4b82ec8f56e54d
Dasom <dev.dasom1012@gmail.com>
2024-08-22 17:19:25 +0900

    catch all route 적용

--- news-project/app/archive/@archive/[year]/page.js
+++
@@ -1,7 +0,0 @@
-import { getNewsForYear } from '@/lib/news';
-import NewsList from '@/components/news-list';
-
-export default function FilteredNewsPage({ params }) {
-  const news = getNewsForYear(params.year);
-  return <NewsList news={news} />;
-}
